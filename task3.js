d315f2f1bcc6d2ce89d7f5e171f5d9a29f2bc77e
laurrain <tchebs.tchebayou1@gmail.com>
2015-02-18 15:01:21 +0200

    task3 is done

---
+++ task3.js
@@ -0,0 +1,9 @@
+//javascript code goes here at the bottom to ensure the DOM is loaded...
+    function GreenFunction() {
+    var x = document.getElementById("white").style.backgroundColor = "green";
+  }
+    function RedFunction(){
+    var y = document.getElementById("white").style.backgroundColor = "red";
+    //document.getElementById("demo").innerHTML = x;
+}
+   
